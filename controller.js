1b5c6d3a22b5d64b1e8f6db1be6776417f1f41f4
zhoriefzainulmuttaqin <zhoriefzainulmuttaqin@gmail.com>
2023-01-02 11:15:50 +0700

    berhasil menampilkan data berdasarkan id

--- controller.js
+++ controller.js
@@ -12,9 +12,23 @@ exports.index = function (req, res) {
 exports.tampilsemuamahasiswa = function (req, res) {
     connection.query('SELECT * FROM  mahasiswa', function (error, rows, fields) {
        if (error) {
-            connection.log(error);
+            console.log(error);
        } else {
             response.ok(rows, res)
        } 
     });
 };
+
+//menampilkan semua data mahasiswa berbasarkan id 
+exports.tampilberdasarkanid = function (req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok(rows, res)
+            }
+        }
+    )
+}
