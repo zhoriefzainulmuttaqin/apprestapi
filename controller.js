8a6417821625f50605c3d9ae213f6cb667035c74
zhoriefzainulmuttaqin <zhoriefzainulmuttaqin@gmail.com>
2023-01-02 13:21:22 +0700

    berhasil menambah data

--- controller.js
+++ controller.js
@@ -32,3 +32,20 @@ exports.tampilberdasarkanid = function (req, res) {
         }
     )
 }
+
+// menambahkan data mahasiswa
+exports.tambahMahasiswa = function (req, res) {
+    let nim = req.body.nim;
+    let nama = req.body.nama;
+    let jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO mahasiswa (nim, nama, jurusan) VALUES(?,?,?)', 
+        [nim,nama,jurusan],
+        function (error) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Menambahkan Data!", res);
+            }
+        });
+};
